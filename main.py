15999f01865460b4a44e2e06f3be3b473d641726
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-17 02:03:43 +0900

    edited main.py to create export, import, logs directory if not exist edited logging configuration to log to files

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 import logging
+import os
+from datetime import datetime
 
 from trainerbuilder import TrainerBuilder
 
-logging.basicConfig(level=logging.DEBUG)
+LOG_DIR = "logs"
+EXPORT_DIR = "export"
+IMPORT_DIR = "import"
+
+def create_log_dir_if_not_exist():
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
+
+def create_export_dir_if_not_exist():
+    if not os.path.exists(EXPORT_DIR):
+        os.makedirs(EXPORT_DIR)
+        
+def create_import_dir_if_not_exist():
+    if not os.path.exists(IMPORT_DIR):
+        os.makedirs(IMPORT_DIR)
+
+
+def get_log_filepath():
+    extension = ".log"
+    current_date_time = datetime.now()
+    filename = current_date_time.strftime('%Y-%m-%d') + extension
+    return os.path.join(LOG_DIR, filename)
+
+
+create_log_dir_if_not_exist()
+create_export_dir_if_not_exist()
+create_import_dir_if_not_exist()
+
+logging.basicConfig(filename=get_log_filepath(),
+                    level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
