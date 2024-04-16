7bb0509506d1dd39ef83c1597ef482c479733d53
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-16 12:31:20 +0900

    added is_valid_json_file

--- jsonfileloader.py
+++ jsonfileloader.py
@@ -7,3 +7,12 @@ logger = logging.getLogger(__name__)
 def load_json_file(filename):
     with open(filename) as file:
         return json.load(file)
+
+
+def is_valid_json_file(filepath):
+    try:
+        with open(filepath, 'r') as file:
+            json.load(file)
+        return True
+    except (json.JSONDecodeError, FileNotFoundError):
+        return False
