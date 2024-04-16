642b49ea026ff3e4aecceb5610da193fa571dfcc
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-16 12:32:12 +0900

    added ImportTrainerCommand execute method

--- commands/misc.py
+++ commands/misc.py
@@ -1,8 +1,11 @@
 import json
 import os
 
+import inquirer
+
 from commands.interfaces import Command
 from exceptions import TrainerBuilderCloseException
+from jsonfileloader import is_valid_json_file
 
 
 class PrintTrainerCommand(Command):
@@ -12,15 +15,35 @@ class PrintTrainerCommand(Command):
 
 class ExportTrainerCommand(Command):
     def execute(self, trainer):
-        export = "export"
+        export_dir = "export"
         filename = trainer.name + ".json"
-        filepath = os.path.join(export, filename)
+        filepath = os.path.join(export_dir, filename)
 
-        if not os.path.exists(export):
-            os.makedirs(export)
+        if not os.path.exists(export_dir):
+            os.makedirs(export_dir)
 
         with open(filepath, "w") as file:
             json.dump(trainer.dict(), file, indent=4)
+
+
+class ImportTrainerCommand(Command):
+    def execute(self, trainer):
+        valid_json_files = self._get_valid_json_files()
+        answer = inquirer.prompt([inquirer.List("file", "Select to import", valid_json_files)])
+        print(answer["file"])  # TODO
+
+    def _get_valid_json_files(self):
+        import_dir = "import"
+        filesets = [self._get_fileset(import_dir, f) for f in os.listdir(import_dir)]
+        return list(filter(self._is_valid_json_file, filesets))
+
+    def _get_fileset(self, import_dir, file):
+        filepath = os.path.join(import_dir, file)
+        return file, filepath
+
+    def _is_valid_json_file(self, fileset):
+        filepath = fileset[1]
+        return is_valid_json_file(filepath)
 
 
 class CloseTrainerBuilderCommand(Command):
