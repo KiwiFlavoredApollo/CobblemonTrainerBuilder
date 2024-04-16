362d8a201818b366a71eccb039bb8d0d57554657
KiwiFlavoredApollo <fractalbarista@gmail.com>
2024-04-16 20:07:57 +0900

    removed dict() method and made property attribute public added _print_and_log_import_message

--- trainer.py
+++ trainer.py
@@ -18,19 +18,16 @@ class Trainer:
 
     def __init__(self, name):
         self.name = name
+        self.properties = load_json_file(self.DEFAULT_TRAINER_FILENAME)
 
         self._logger = logging.getLogger(__name__)
-        self._properties = load_json_file(self.DEFAULT_TRAINER_FILENAME)
 
     def add_pokemon_to_team(self, pokemon):
-        self._properties[self.TEAM].append(pokemon)
-
-    def dict(self):
-        return self._properties
+        self.properties[self.TEAM].append(pokemon)
 
     def get_pokemon_names(self):
         names = []
-        for pokemon in self._properties[self.TEAM]:
+        for pokemon in self.properties[self.TEAM]:
             n = self._remove_cobblemon_prefix_from_pokemon_name(pokemon["species"])
             names.append(n)
         return names
@@ -39,34 +36,38 @@ class Trainer:
         return name.replace(PokemonBuilder.COBBLEMON_PREFIX, "")
 
     def set_win_command(self, win_command):
-        self._properties[self.WIN_COMMAND] = win_command
+        self.properties[self.WIN_COMMAND] = win_command
 
     def set_can_only_beat_once(self, can_only_beat_once):
-        self._properties[self.CAN_ONLY_BEAT_ONCE] = can_only_beat_once
+        self.properties[self.CAN_ONLY_BEAT_ONCE] = can_only_beat_once
 
     def reset(self):
-        self._properties = load_json_file(self.DEFAULT_TRAINER_FILENAME)
+        self.properties = load_json_file(self.DEFAULT_TRAINER_FILENAME)
 
     def remove_pokemon(self, slot):
         try:
             self._assert_slot_exist_pokemon(slot)
-            self._properties[self.TEAM].pop(slot)
+            self.properties[self.TEAM].pop(slot)
         except PokemonNotExistSlotException:
             pass
 
     def _assert_slot_exist_pokemon(self, slot):
-        maximum_slot = len(self._properties[self.TEAM]) - 1
+        maximum_slot = len(self.properties[self.TEAM]) - 1
         if maximum_slot < slot:
             raise PokemonNotExistSlotException
 
     @property
     def team(self):
-        return self._properties[self.TEAM]
+        return self.properties[self.TEAM]
 
     def import_from_json_file(self, filepath):
         try:
-            self._properties = load_json_file(filepath)
-            self._logger.debug("Imported {}".format(filepath))
+            self.properties = load_json_file(filepath)
+            self._print_and_log_import_message(filepath)
         except FileNotFoundError:
             self._logger.debug("Following file could not be found: " + self.DEFAULT_TRAINER_FILENAME)
 
+    def _print_and_log_import_message(self, filepath):
+        message = "Imported from {}".format(filepath)
+        print(message)
+        self._logger.debug(message)
